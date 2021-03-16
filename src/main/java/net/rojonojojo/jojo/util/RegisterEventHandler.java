5ba9ce40b1cd14dd3be9ff76399caf65413e42fb
SaiSamarth-1 <pgubbil@gmail.com>
2021-03-16 12:25:54 +0530

    started hamon, fixed Event handler for registry

--- src/main/java/net/rojonojojo/jojo/util/RegisterEventHandler.java
+++ src/main/java/net/rojonojojo/jojo/util/RegisterEventHandler.java
@@ -22,18 +22,18 @@ import java.util.Map;
 
 import static xyz.heroesunited.heroesunited.util.HUTickrate.CLIENT_TICK;
 
-@Mod.EventBusSubscriber(modid = RojoNoJojo.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
-public class ClientEventBusSubscriber {
+@Mod.EventBusSubscriber(modid = RojoNoJojo.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class RegisterEventHandler {
 
     public static final Map<String, AbilityType> ABILITIES = new HashMap<>();
 
     public static final AbilityType HEALING = new AbilityType(Healing::new, RojoNoJojo.MOD_ID, "healing");
 
 
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event){
-
-    }
+//    @SubscribeEvent
+//    public static void onClientSetup(FMLClientSetupEvent event){
+//
+//    }
 
 
     @SubscribeEvent
@@ -42,24 +42,24 @@ public class ClientEventBusSubscriber {
         ABILITIES.put(HEALING.getRegistryName().toString(),HEALING);
     }
 
-    @SubscribeEvent
-    public static void onWorldTick(TickEvent.WorldTickEvent event) {
-        float tickrate = -20.0f;
+//    @SubscribeEvent
+//    public static void onWorldTick(TickEvent.WorldTickEvent event) {
+//        float tickrate = -20.0f;
+//
+//        if (event.side.isClient() && CLIENT_TICK != tickrate) {
+//            ObfuscationReflectionHelper.setPrivateValue(Minecraft.class, Minecraft.getInstance(), new Timer(tickrate, 0l), "timer");
+//            CLIENT_TICK = tickrate;
+//        }
+//        if (event.side.isServer() && HUTickrate.SERVER_TICK != (long) (1000L / tickrate)) {
+//            HUTickrate.SERVER_TICK = (long) (1000L / tickrate);
+//
+//        }
+//
+//    }
 
-        if (event.side.isClient() && CLIENT_TICK != tickrate) {
-            ObfuscationReflectionHelper.setPrivateValue(Minecraft.class, Minecraft.getInstance(), new Timer(tickrate, 0l), "timer");
-            CLIENT_TICK = tickrate;
-        }
-        if (event.side.isServer() && HUTickrate.SERVER_TICK != (long) (1000L / tickrate)) {
-            HUTickrate.SERVER_TICK = (long) (1000L / tickrate);
-
-        }
-
-    }
-
-    @SubscribeEvent
-    public static void playerTick(TickEvent.PlayerTickEvent event){
-        //event.player.sendMessage(new StringTextComponent("sip"), Util.NIL_UUID);
-    }
+//    @SubscribeEvent
+//    public static void playerTick(TickEvent.PlayerTickEvent event){
+//        //event.player.sendMessage(new StringTextComponent("sip"), Util.NIL_UUID);
+//    }
 
 }
